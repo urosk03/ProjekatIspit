b2f2714e53c8d3934069248a262fdef627ee89ca
urosk03 <142893818+urosk03@users.noreply.github.com>
2025-09-04 14:16:32 +0200

    Delete coworking-hub (1) directory

--- coworking-hub (1)/src/main/java/com/example/coworkinghub/model/ReservationStatus.java
+++
@@ -1,2 +0,0 @@
-package com.example.coworkinghub.model;
-public enum ReservationStatus { PENDING, APPROVED, REJECTED, CANCELLED }
--- coworking-hub (1)/src/main/java/com/example/coworkinghub/model/Role.java
+++
@@ -1,2 +0,0 @@
-package com.example.coworkinghub.model;
-public enum Role { MEMBER, MANAGER }
